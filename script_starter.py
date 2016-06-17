8de1337467fbee76c1b5792a8715a561127500bc
Joakim Bang <jocke.bang@gmail.com>
2016-06-17 17:42:40 +1000

    new log

--- script_starter.py
+++ script_starter.py
@@ -3,6 +3,9 @@ from time import ctime, sleep
 from os import system
 
 sleep(60)
+with open('/home/joakim/work/log.log', 'wb') as logFile:
+	logFile.write(ctime() + ':\t Starting log.\n')
+
 system('supervisorctl -c /home/joakim/work/scraper/supervisord.conf restart all')
 with open('/home/joakim/work/restarts.log', 'ab') as logFile:
 	logFile.write(ctime() + ':\t Logged in and restarted supervisor.\n')
