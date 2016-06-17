a5e1c75d48174ed88faa7ee7a222d4c9087dea06
joakm-bang <jocke.bang@gmail.com>
2016-06-17 20:55:02 +1000

    >>

--- startscraping.sh
+++ startscraping.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo "Staring to scarpe: $(date)" > /home/joakim/work/t3.log
+echo "Waiting for system to get ready: $(date)" > /home/joakim/work/t3.log
+python /home/joakim/work/scraper/ready.py
+echo "Staring to scarpe: $(date)" >> /home/joakim/work/t3.log
 cd /home/joakim/work/scraper/
 sudo supervisord -n -c supervisord.conf
 exit 0
