405142fbce6fa827302de0fad1c3628acc2b5174
Joakim Bang <jocke.bang@gmail.com>
2016-06-23 12:04:16 +1000

    more months

--- scraper.py
+++ scraper.py
@@ -205,7 +205,7 @@ class Settings:
 			self.scrapeMonths = True
 			#self.scraped = False
 			self.ll = 3000001
-			self.ul = 3600000
+			self.ul = 4000000
 		elif self.computer == 'vbox12':   # Vbox12  (logs, , [300 001, 600 000])
 			self.runLAN = True
 			self.bannedIP = '60.241.126.187'
