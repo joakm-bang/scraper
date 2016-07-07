14d8974daa2df415d6d1f0cc1b211e7ea55c88a2
root <root@vbox16.(none)>
2016-07-07 15:00:10 +1000

    HP

--- startscrapingHP.sh
+++ startscrapingHP.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo "Starting supervisor: $(date)" > /home/joakim/work/t.log
+echo "Taking nap: $(date)" > /home/joakim/work/t.log
+sleep 180
+echo "Starting supervisor: $(date)" >> /home/joakim/work/t.log
 cd ~/work/scraper/
 sudo supervisord -n -c supervisord.conf
 exit 0
