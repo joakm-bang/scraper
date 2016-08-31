37d4d0420b7564416c28ceda05ed5f7e648cd836
joakm-bang <jocke.bang@gmail.com>
2016-08-31 09:47:51 +0200

    IP

--- ready.py
+++ ready.py
@@ -33,7 +33,7 @@ while not done:
 		#15 minutes already. Try bouncing it.
 		system('sudo reboot')
 	ip = getIP()
-	ping = system('ping -c 1 192.168.0.2')
+	ping = system('ping -c 1 192.168.0.168')
 	if ip != '60.241.126.187' and ping == 0:
 		done = True
 	else:
